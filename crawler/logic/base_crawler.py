ad7ce80cb2785cd14b81f937ec161268917ce26a
Sbtah <shisastore@protonmail.com>
2023-01-23 20:47:57 +0100

    Fixed docstrings, moved method to child class.

--- crawler/logic/base_crawler.py
+++ crawler/logic/base_crawler.py
@@ -1,11 +1,9 @@
-import asyncio
 from random import choice
 from typing import List
 
 import httpx
 
 from crawler.helpers.logger import logger
-from crawler.options.endpoints import SINGLE_PRODUCT_ENDPOINT
 from crawler.options.settings import USER_AGENTS
 
 
@@ -19,7 +17,10 @@ class BaseApiCrawler:
 
     @classmethod
     def get_random_user_agent(cls, user_agent_list: List[str]) -> str:
-        """Return random User-Agent"""
+        """
+        Return str with random User-Agent.
+        - :arg user_agent_list: List of strings with User Agents.
+        """
         agent = choice(user_agent_list)
         return agent
 
@@ -31,7 +32,7 @@ class BaseApiCrawler:
     def get(self, url: str) -> str:
         """
         Requests specified url synchronously. Returns JSON.
-        - :param url: Requested URL.
+        - :arg url: Requested URL.
         """
         headers = {"User-Agent": f"{self.user_agent}"}
         try:
@@ -48,39 +49,18 @@ class BaseApiCrawler:
             return None
         except Exception as e:
             self.logger.error(f"(get) Exception: {e}")
+            return None
 
     async def async_get(self, client: httpx.AsyncClient, url: str) -> str:
         """
         Requests specified URL asynchronously. Returns JSON.
-        - :param client: Asynchronous client.
-        - :param url: Requested URL.
+        - :arg client: Asynchronous client.
+        - :arg url: Requested URL.
         """
         headers = {"User-Agent": f"{self.user_agent}"}
         try:
             res = await client.get(url, headers=headers)
             return res.json()
         except Exception as e:
-            self.logger.error(f"(async get) Exception: {e}")
+            self.logger.error(f"(async_get) Exception: {e}")
             return None
-
-    async def search_products_by_ids(self, range_of_ids):
-        """
-        Sends requests to SINGLE_PRODUCT_ENDPOINT,
-        if there is a response we found a product.
-        """
-
-        async with httpx.AsyncClient() as client:
-
-            tasks = []
-            for num in range_of_ids:
-                tasks.append(
-                    asyncio.ensure_future(
-                        self.async_get(
-                            client,
-                            SINGLE_PRODUCT_ENDPOINT.format(num),
-                        )
-                    )
-                )
-
-            products = await asyncio.gather(*tasks)
-            return products
