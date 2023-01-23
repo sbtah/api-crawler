f521d5252244af389fa3d5364e3c30e29c7b2f1a
Sbtah <shisastore@protonmail.com>
2023-01-23 20:45:56 +0100

    Modifiy doc strings

--- crawler/logic/api_crawler.py
+++ crawler/logic/api_crawler.py
@@ -4,7 +4,7 @@ from typing import Dict, Iterator, List
 import httpx
 
 from crawler.logic.base_crawler import BaseApiCrawler
-from crawler.options.endpoints import SINGLE_PRODUCT_ENDPOINT
+from crawler.options.endpoints import SINGLE_PRODUCT_BY_ID
 
 
 class ApiCrawler(BaseApiCrawler):
@@ -16,7 +16,11 @@ class ApiCrawler(BaseApiCrawler):
         super().__init__(*args, **kwargs)
 
     def get_single_product_by_id(self, product_id):
-        """"""
+        """
+        Requests SINGLE_PRODUCT_BY_ID endpoint synchronously.
+        Returns JSON with Product data.
+        - :arg product_id: Integer that will represent ID of Product.
+        """
         pass
 
     async def discover_products_by_ids(
@@ -24,9 +28,9 @@ class ApiCrawler(BaseApiCrawler):
         range_of_ids: Iterator[int],
     ) -> Dict:
         """
-        Sends requests to SINGLE_PRODUCT_ENDPOINT,
+        Sends requests to SINGLE_PRODUCT_BY_ID,
         if there is a response we found a product.
-        - :param range_of_ids: Iterator of integers that will be looped over,
+        - :arg range_of_ids: Iterator of integers that will be looped over,
             to search for products.
         """
         async with httpx.AsyncClient() as client:
@@ -37,10 +41,13 @@ class ApiCrawler(BaseApiCrawler):
                     asyncio.ensure_future(
                         self.async_get(
                             client,
-                            SINGLE_PRODUCT_ENDPOINT.format(num),
+                            SINGLE_PRODUCT_BY_ID.format(num),
                         )
                     )
                 )
 
             products = await asyncio.gather(*tasks)
             return products
+
+    async def get_product_data_for_local_store():
+        pass
