3e29d934e28b506204e1f3d05bf93a6c3afbaef3
Sbtah <shisastore@protonmail.com>
2023-01-23 20:48:14 +0100

    Small tweaks

--- crawler/options/endpoints.py
+++ crawler/options/endpoints.py
@@ -4,7 +4,4 @@ from dotenv import load_dotenv
 
 # Get private data fron .env variable
 load_dotenv()
-
-LOCAL_STORES_ENDPOINT = os.environ.get("LOCAL_STORES_ENDPOINT")
-HOME_PAGE_ENDPOINT = os.environ.get("HOME_PAGE_ENDPOINT")
-SINGLE_PRODUCT_ENDPOINT = os.environ.get("SINGLE_PRODUCT_ENDPOINT")
+SINGLE_PRODUCT_BY_ID = os.environ.get("SINGLE_PRODUCT_BY_ID")
--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import asyncio
-
+import time
 from crawler.helpers.logger import logger
 from crawler.helpers.mapper import generate_ids_map
 from crawler.helpers.time_it import calculate_time
-from crawler.logic.base_crawler import BaseApiCrawler
-from crawler.options.endpoints import SINGLE_PRODUCT_ENDPOINT
+from crawler.logic.api_crawler import ApiCrawler
+from crawler.options.endpoints import SINGLE_PRODUCT_BY_ID
 
 
 @calculate_time
-def search_all_products():
+def search_for_all_products_async():
     """"""
-    crawler = BaseApiCrawler()
+    crawler = ApiCrawler()
     genex = generate_ids_map()
 
     for generator in genex:
-        products = asyncio.run(crawler.search_products_by_ids(range_of_ids=generator))
+        products = asyncio.run(crawler.discover_products_by_ids(range_of_ids=generator))
         for x in products:
             if x:
-                pid = x["data"]["entity_id"]
-                print(pid)
+                with open("ids_list.txt", "a") as file1:
+                    pid = x["data"]["entity_id"]
+                    print(pid)
+                    file1.write(f"\n{pid}")
             else:
                 print(f"{x}...")
 
 
 if __name__ == "__main__":
-    search_all_products()
+    search_for_all_products_async()
