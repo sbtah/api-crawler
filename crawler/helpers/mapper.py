75d37c3adcfda63c91f3e25b3eb0ed14b688136d
Sbtah <shisastore@protonmail.com>
2023-01-23 20:46:48 +0100

    Rework mapper function.

--- crawler/helpers/mapper.py
+++ crawler/helpers/mapper.py
@@ -1,9 +1,8 @@
 def generate_ids_map():
     """
     Return generator of generators.
-    Used for creating intigers that will be used as products IDs.
+    Used for creating integers that will be used as products IDs.
     """
     stp = 100
-    genex = ((y for y in range(x, stp + x)) for x in range(104077, 2500000, stp))
-
+    genex = ((y for y in range(x, stp + x)) for x in range(1, 2500000, stp))
     return genex
